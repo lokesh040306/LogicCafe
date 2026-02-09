50a6584f5c33c6a82f23f89e08c47f7043f466a8
Lokesh Dilip Bandurkar <lokeshdan770@gmail.com>
2026-02-09 14:57:40 +0530

    .

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,7 +6,7 @@ import { getToken } from "./auth.service";
  * Backend base URL
  */
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api` || "http://localhost:5000/api",
   timeout: 10000,
 });
 
